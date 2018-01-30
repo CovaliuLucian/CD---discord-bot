66e928f6cf146d95e9777ee8297ab911eb692906
Covaliu Lucian <luciancovaliu41@gmail.com>
2018-01-30 18:09:45 +0200

    Token

---
+++ CD/CD/Token.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace CD
+{
+    class Token
+    {
+        private readonly string token;
+
+        Token()
+        {
+            token = System.IO.File.ReadLines("token.txt").First();
+        }
+    }
+}
